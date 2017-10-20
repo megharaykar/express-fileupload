a5aa124ffe2eb0be4d4bee7bf0bca270c41ee12f
megharaykar <megharaykar24@gmail.com>
2017-10-20 12:59:46 -0700

    first-commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+    app = express(),
+    http = require("http").Server(app).listen(3002),
+    upload = require("express-fileupload"),
+    bodyParser = require('body-parser'),
+    path = require('path');
+app.use(upload())
+
+console.log("server started")
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/index.html");
+})
+app.post("/", function(req,res){
+    if(req.files){
+        var file = req.files.filename,
+            filename = file.name;
+        file.mv("./upload/"+filename,function(err){
+            if(err){
+                console.log(err)
+                res.send("error occured")
+            }
+            else{
+                res.send("Done!")
+            }
+        })
+    }
+})
---
+++ upload/service.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+print "loading function"
+
+def handler(event, context):
+    # Your code goes here!
+	print "Hello!"
+    #e = event.get('e')
+    #pi = event.get('pi')
+    return "OK"
